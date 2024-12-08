a93d0073b656d9a38b11185e170103eab018e8c3
Alexis <marcoesparza973@gmail.com>
2024-12-07 19:01:02 -0600

    fondo

--- ganado-front/src/Home.jsx
+++ ganado-front/src/Home.jsx
@@ -3,14 +3,15 @@ import { Navbar } from "./components/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 
+
 const Home = () => {
   
   return (
     <>
       <Navbar />
       <main className="flex-shrink-0">
-        <div className="container">
-          <div className="d-flex justify-content-center alig flex-column text-center">
+        <div className="container1">      
+         <div className="d-flex justify-content-center alig flex-column text-center">
           <h1 className="mt-5">Bienvenido al Sistema de Control de Granja el Charolais</h1>
 
           <img src="/logo.jpg" alt="" className="home-bg mt-2 m-auto mb-2"/>
